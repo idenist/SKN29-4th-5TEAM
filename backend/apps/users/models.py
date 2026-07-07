548efb6d114a5a73573c63f9d40b182206a512b0
Ubuntu <ubuntu@ip-10-0-9-172.ap-northeast-2.compute.internal>
2026-07-07 09:50:12 +0000

    community, email update

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from datetime import timedelta
+from django.utils import timezone
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     """
